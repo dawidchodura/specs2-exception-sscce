b122926e32c5796d92837b511ea759e946c45cdb
Dawid Chodura <dawid.chodura@ocado.com>
2019-10-20 23:50:45 +0200

    Minimal example of the failure to report the exception line number

---
+++ src/test/scala/example/ExampleTest.scala
@@ -0,0 +1,18 @@
+package example
+
+import org.specs2.main.ArgProperties
+import org.specs2.mutable.SpecWithJUnit
+import org.specs2.specification.Scope
+
+class ExampleTest extends SpecWithJUnit {
+
+  "Example" should {
+    "report the line number of the exception" in new InnerScope {
+      println("Starting the test")
+      if(true) throw new RuntimeException("Which line did this exception occur at?")
+      true must beTrue
+    }
+  }
+
+  abstract class InnerScope extends Scope {}
+}
